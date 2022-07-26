5e60449c1891749c1f2992fb9fdc9bb7b99f3275
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-07-26 16:45:49 +0430

    the lables has been changed.

--- about.php
+++ about.php
@@ -12,12 +12,12 @@
         <!-- Intro Content -->
         <div class="row" style="margin-bottom:50px;margin-left:50px">
            
-               <P> Employee Feedback system for College 
+    <P> Employee Feedback system for College 
 Here we have developed the a ATRA feedback system, which is generally used in the college to rate the ATRA based on the performance...Here we have 2 modules such as administrator, student.
 
 Administrator is the one who creates the  Employee account by adding all  Employeeinfo and assigning the username and password. 
 Admin als0 checks the result once all  Employee entered the feedback..
-We can start the development from the login page, where we have given the option to login as admin and  Employee ...Here since we have only one admin account, so no need to create the a database to store admin info...so the admin username is "admin" and password is "sandeep"...select admin in the radio button and login 
+We can start the development from the login page, where we have given the option to login as admin and  Employee ...Here since we have only one admin account, so no need to create the a database to store admin info...so the admin username is "admin" and password is "sandeep"...select admin in the radio button and login so the admin username is "admin" and password is "sandeep"...select admin in the radio button and login 
 
 You can perform all admin actions such as login to the account and check result..
 
@@ -28,7 +28,4 @@ Before we can look into the php code, you need to create a database called "feed
                
             </div>
         </div>
-        <!-- /.row -->
-
-        <!-- Footer -->
      
